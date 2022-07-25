8895c92b3ba0b0a7920880b4f20d0a631df9bc40
Alessandro DESSI <alessandro.dessi@nicecactus.gg>
2022-07-25 18:22:28 +0200

    refactor login

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "../../components/LoginForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
 import "./LoginPage.scss";
 
 const LoginPage: React.FC = () => {
