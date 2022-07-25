146cdb1b8c5801c67a3902f5493b9ba5b3d8aec2
Alessandro DESSI <alessandro.dessi@nicecactus.gg>
2022-07-25 14:52:38 +0200

    hadle login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import Button from "../atoms/Button/Button";
+import TextInput from "../atoms/TextInput/TextInput";
 import Card from "../molecules/Card/Card";
+import { AuthService } from "../services/auth.services";
 import "./LoginForm.scss";
 
 const LoginForm: React.FC = () => {
-    const [ username, setUsername ] = useState<String>("");
-    const [ password, setPassword ] = useState<String>("");
+    const [ username, setUsername ] = useState<string>('');
+    const [ usernameError, setUsernameError ] = useState<string>('');
+    const [ password, setPassword ] = useState<string>('');
+    const [ passwordError, setPasswordError ] = useState<string>('');
     
-    const login = () => {
-        console.log("TBD: login");
-        setUsername("urtoob");
-        setPassword("ToobRU");
+    const login = async () => {
+        setUsernameError(username === '' ? "Please insert the username" : '');
+        setPasswordError(password === '' ? "Please insert the password" : '');
+        if (username && password) {
+            await AuthService.login({identifiant: username, password});
+        }
     }
 
     return (
@@ -18,8 +24,19 @@ const LoginForm: React.FC = () => {
             title={"Hello there!"}
             styleClass="login-form"
             children={<div>
-                <div>{username}</div>
-                <div>{password}</div>
+                <TextInput 
+                    label="username" 
+                    value={username}
+                    setChange={setUsername}
+                    messageError={usernameError}
+                />
+                <TextInput 
+                    label="password" 
+                    value={password}
+                    setChange={setPassword} 
+                    messageError={passwordError}
+                    hideText
+                />
             </div>}
             actions={<Button text="Login" action={login} />}
         />
