9c98c4a0c2e2ad0d1dc99edf7756ab35e0979bc3
Alessandro DESSI <alessandro.dessi@nicecactus.gg>
2022-07-25 14:50:44 +0200

    create text input atom

---
+++ src/atoms/TextInput/TextInput.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './TextInput.scss';
+
+interface TextInputProps {
+    label: string;
+    value: string;
+    messageError?: string;
+    setChange: (value: string) => void;
+    hideText?: boolean
+
+};
+
+const TextInput: React.FC<TextInputProps> = (props: TextInputProps) => {
+    const { messageError } = props;
+
+    const handleChange = (event: { target: { value: string; }; }) => {
+        props.setChange(event.target.value);
+    }
+
+    return (
+        <div className="text-input chi-form__item">
+            <label className="chi-label">{props.label}</label>
+            <input 
+                type={`${props.hideText ? 'password' : 'text'}`} 
+                className={`chi-input ${messageError ? '-danger' : ''}`} 
+                onChange={handleChange}
+            />
+            {messageError && 
+                <div className="chi-label -status -danger">{messageError}</div>
+            }
+        </div>
+    );
+};
+
+export default TextInput;
