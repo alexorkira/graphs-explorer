b6a92407d45b2056a545b8a63adfb9233497a494
Alessandro DESSI <alessandro.dessi@nicecactus.gg>
2022-07-25 16:35:17 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import LoginPage from './pages/Login/LoginPage';
+import ContextStore from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <LoginPage />
+const App = () => {
+    const sessionToken = ContextStore.useStoreState((store) => store.sessionToken);
+
+    return (
+      <div className="App">
+        <Router basename='/'>
+            <Switch>
+                {sessionToken 
+                    ?<Route path="/graphs" exact><div>TBD: Show the graphs</div></Route>
+                    : <Route path="/login" exact component={LoginPage} />
+                }
+                <Route path="/">
+                    <Redirect to={`${sessionToken ? '/graphs' : '/login'}`} />
+                </Route>
+            </Switch>
+        </Router>
     </div>
-  );
+);
+
 }
 
 export default App;
